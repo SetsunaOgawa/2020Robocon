867e1a83f36e31e2bbea04ddbb89541ae82ce559
SetsunaOgawa <70934292+SetsunaOgawa@users.noreply.github.com>
2020-09-08 14:50:28 +0900

    Update roboTest.cpp

--- roboTest.cpp
+++ roboTest.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 
-double vel=0;//速度
-double rad=0;//旋回半径
-double tang=0;//目標角
-double width=147;//車幅(mm)
-double cang=0;//中心角
-double ave=0;//角速度
+double vel=50;
+double rad=500;
+double tang=-645;
+double width=147;
+double cang=0;
+double ave=0;
 double vr=0;
 double vl=0;
 double dr=0;
 double dl=0;
-double pi = 3.14;
-
-int main() {
-    ave = vel/rad;
-    cang = 2*tang*pi/180;
-    if(cang < 0){
-        vr = vel+( width / 2 )*ave;
-        vl = vel-( width / 2 )*ave;
-        cang = -cang;
-    }
-    else if(cang > 0){
-        vr = vel-( width / 2 )*ave;
-        vl = vel+( width / 2 )*ave;
-    }
-    else{
-        vr = vel;
-        vl = vel;
-    }
-    dr = vr * cang / ave;
-    dl = vl * cang / ave;
+double pi=3.14;
+int main(){
+	ave = vel/rad;
+	cang = 2*tang*pi/180;
+	if(cang<0){
+		vr = vel+(width/2)*ave;
+		vl = vel-(width/2)*ave;
+		cang = -cang;
+	}
+	else if(cang>0){
+		vr = vel-(width/2)*ave;
+		vl = vel+(width/2)*ave;
+	}
+	else{
+		vr = vel;
+		vl = vel;
+	}
+	dr = vr*cang/ave;
+	dl = vl*cang/ave;
+	std::cout<<vr<<std::endl;
+	std::cout<<vl<<std::endl;
+	std::cout<<dr<<std::endl;
+	std::cout<<dl<<std::endl;
+	std::cout<<cang/ave<<std::endl;
 }
